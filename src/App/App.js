9aa8e3114a37e3a9708cf609b0b5a8ffbc23b402
Di Wang <jackiewang5566@hotmail.com>
2017-08-07 23:41:35 -0400

    scaffold Tap-News structure via create-react-app

---
+++ src/App/App.js
@@ -0,0 +1,21 @@
+// import '../../node_modules/materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/css/materialize.min.css';
+
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <img className='logo' src={logo} alt='logo' />
+                <div className='container'>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
