b68982808df5bd167d5afb997748147000f10bd4
Di Wang <jackiewang5566@hotmail.com>
2017-08-08 01:47:05 -0400

    using express to scaffold server side structure, add get news router

---
+++ web_server/server/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var news = require('./routes/news');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, '../client/build/'));
+app.set('view engine', 'jade');
+app.use('/static', express.static(path.join(__dirname, '../client/build/static/')));
+
+
+app.use('/', index);
+app.use('/news', news);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.send('404 Not Found');
+});
+
+module.exports = app;
