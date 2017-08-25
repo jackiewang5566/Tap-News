59262632db50352318f9921e66445df811404543
Di Wang <jackiewang5566@hotmail.com>
2017-08-25 00:05:33 -0400

    add helper script for clearing the queue in case there are some unformatted news accidentally added into queue(for develop only)

---
+++ news_pipeline/queue_helper.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+# import common package in parent directory
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
+
+import news_api_client
+from cloudAMQP_client import CloudAMQPClient
+
+SCRAPE_NEWS_TASK_QUEUE_URL = "amqp://mcyrgohw:CB44sIsZxuz-IInG5a5ESFGrnP0iIda4@crane.rmq.cloudamqp.com/mcyrgohw"
+SCRAPE_NEWS_TASK_QUEUE_NAME = "tap-news-scrape-news-task-queue"
+
+
+def clearQueue(queue_url, queue_name):
+    scrape_news_queue_client = CloudAMQPClient(queue_url, queue_name)
+
+    num_of_messages = 0
+
+    while True:
+        if scrape_news_queue_client is not None:
+            msg = scrape_news_queue_client.get_message()
+            if msg is None:
+                print "Clear %d messages." % num_of_messages
+                return
+            num_of_messages += 1
+
+
+if __name__ == "__main__":
+    clearQueue(SCRAPE_NEWS_TASK_QUEUE_URL, SCRAPE_NEWS_TASK_QUEUE_NAME)
