cfea31ae1fd677cf55847b630e387dc434b93a6e
Di Wang <jackiewang5566@hotmail.com>
2017-08-26 19:21:59 -0400

    change news router accordingly and add getNewsSummariesForUser in rpc_client to connect node service with backend service

--- web_server/server/rpc_client/rpc_client.js
+++ web_server/server/rpc_client/rpc_client.js
@@ -10,11 +10,22 @@ var client = jayson.client.http({
 function add(a, b, callback) {
     client.request('add', [a, b], function (err, error, response) {
         if (err) throw err;
-	console.log(response);
-	callback(response);
+        console.log(response);
+        callback(response);
     })
 }
 
+// Get news summaries for a user
+function getNewsSummariesForUser(user_id, page_num, callback) {
+    client.request('getNewsSummariesForUser', [user_id, page_num], function (err, error, response) {
+        if (err) throw err;
+        console.log(response);
+        callback(response);
+    })
+}
+
+
 module.exports = {
-    add: add
+    add: add,
+    getNewsSummariesForUser: getNewsSummariesForUser
 };
