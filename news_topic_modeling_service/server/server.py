f1c19cc5788da13625586542415f1f9c88c376f3
Di Wang <jackiewang5566@hotmail.com>
2017-09-02 01:05:08 -0400

    add news_classes map to number to topic and news_topic_modeling_service_client script and its test

--- news_topic_modeling_service/server/server.py
+++ news_topic_modeling_service/server/server.py
@@ -16,6 +16,8 @@ from watchdog.events import FileSystemEventHandler
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'trainer'))
 import news_cnn_model
 
+learn = tf.contrib.learn
+
 SERVER_HOST = 'localhost'
 SERVER_PORT = 6060
 
@@ -38,9 +40,11 @@ def restoreVars():
     with open(VARS_FILE, 'r') as f:
         global n_words
         n_words = pickle.load(f)
-        vocab_processor = learn.preprocessing.VocabularyProcessor.restore(
-            VOCAB_PROCESSOR_SAVE_FILE
-        )
+        
+    global vocab_processor
+    vocab_processor = learn.preprocessing.VocabularyProcessor.restore(
+        VOCAB_PROCESSOR_SAVE_FILE
+    )
 
 def loadModel():
     global classifier
@@ -53,6 +57,56 @@ def loadModel():
 
     # TODO: fix this until https://github.com/tensorflow/tensorflow/issues/5548 is solved.
     # We have to call evaluate or predict at least once to make the restored Estimator work.
-    train_df = df[0]
+    train_df = df[0:400]
+    x_train = train_df[1]
+    x_train = np.array(list(vocab_processor.transform(x_train)))
+    y_train = train_df[0]
+    classifier.evaluate(x_train, y_train)
 
-class RequestHandler(pyjsonrpc.HttpRequesthandler):
+    print "Model update."
+
+restoreVars()
+loadModel()
+
+print "Model loaded."
+
+# watchdog needed
+class ReloadModelHandler(FileSystemEventHandler):
+    def on_any_event(self, event):
+        # Reload model
+        print "Model update detected. Loading new model."
+        time.sleep(MODEL_UPDATE_LAG_IN_SECONDS)
+        restoreVars()
+        loadModel()
+
+class RequestHandler(pyjsonrpc.HttpRequestHandler):
+    @pyjsonrpc.rpcmethod
+    def classify(self, text):
+        text_series = pd.Series([text])
+        predict_x = np.array(list(vocab_processor.transform(text_series)))
+        print predict_x
+
+        y_predicted = [
+            p['class'] for p in classifier.predict(
+                predict_x, as_iterable=True
+            )
+        ]
+        print y_predicted[0]
+        topic = news_classes.class_map[str(y_predicted[0])]
+        return topic
+
+# Setup watchdog
+observer = Observer()
+observer.schedule(ReloadModelHandler(), path=MODEL_DIR, recursive=False)
+observer.start()
+
+# Threading HTTP-Server
+http_server = pyjsonrpc.ThreadingHttpServer(
+    server_address = (SERVER_HOST, SERVER_PORT),
+    RequestHandlerClass = RequestHandler
+)
+
+print "Starting predicting server ..."
+print "URL: http://" + str(SERVER_HOST) + ":" + str(SERVER_PORT)
+
+http_server.serve_forever()
