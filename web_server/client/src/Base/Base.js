e84105168bfa108f00a33cfb8b890292b66ab2fa
Di Wang <jackiewang5566@hotmail.com>
2017-09-16 11:14:54 -0400

    cosmetic change, add some paddings on navbar

--- web_server/client/src/Base/Base.js
+++ web_server/client/src/Base/Base.js
@@ -4,23 +4,25 @@ import { Link } from 'react-router';
 import './Base.css';
 
 const Base = ({ children }) => (
-    <div>
+    <div className="row">
         <nav className="nav-bar indigo lighten-1">
             <div className="nav-wrapper">
-                <a href="/" className="brand-logo"> Tap News</a>
-                <ul id="nav-mobile" className="right">
-                    {Auth.isUserAuthenticated() ? 
-                        (<div>
-                            <li>{Auth.getEmail()}</li>
-                            <li><Link to="/logout">Log out</Link></li>
-                        </div>) 
-                        : 
-                        (<div>
-                            <li><Link to="/login">Log in</Link></li>    
-                            <li><Link to="/signup">Sign up</Link></li>    
-                        </div>)
-                    }
-                </ul>
+                <div className="col s12">
+                    <a href="/" className="brand-logo"> Tap News</a>
+                    <ul id="nav-mobile" className="right">
+                        {Auth.isUserAuthenticated() ? 
+                            (<div>
+                                <li>{Auth.getEmail()}</li>
+                                <li><Link to="/logout">Log out</Link></li>
+                            </div>) 
+                            : 
+                            (<div>
+                                <li><Link to="/login">Log in</Link></li>    
+                                <li><Link to="/signup">Sign up</Link></li>    
+                            </div>)
+                        }
+                    </ul>
+                </div>
             </div>
         </nav>
         <br/>
